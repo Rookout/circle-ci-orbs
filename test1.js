30d8c82ff6ae571c28f051dd90bc115af78d85f2
BENROOKOUT\Ben <ben@rookout.com>
2018-12-04 12:37:29 +0200

    a

---
+++ test1.js
@@ -0,0 +1,26 @@
+console.log(new Date(Date.now()).toLocaleString() +  " HelloWorld");
+
+const animals = ["Aardvark",
+    "Abyssinian",
+    "Affenpinscher",
+    "Akbash",
+    "Zebu",
+    "Zonkey",
+    "Zorseqdwewqeqweqweqweqw"];
+	
+function a() {
+	let animal = animals[Math.floor(Math.random()*animals.length)];
+	console.log(animal);
+}
+
+var index = 0;
+
+setInterval(() => {
+        console.log(new Date(Date.now()).toLocaleString() + " iteration");
+		a();
+		
+		index = index + 1;
+		if (index == 10){
+			process.exit(0);
+		}
+    }, 5000);
