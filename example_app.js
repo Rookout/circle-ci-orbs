63a3418b43384a319b14161dad2a5ef7d971f844
BENROOKOUT\Ben <ben@rookout.com>
2019-01-09 15:07:32 +0200

    added fake bug

--- example_app.js
+++ example_app.js
@@ -1,19 +1,27 @@
+/*
+	This is a simple JS file that will be debugged using Rookout under circle-ci
+*/
+
 console.log(new Date(Date.now()).toLocaleString() +  " HelloWorld");
 
 const animals = ["Rook",
     "Big Rook",
-    "Small Rook"];
-	
+    "Small Rook",
+	"Bird"];
+
+
 function func() {
 	let animal = animals[Math.floor(Math.random()*animals.length)];
 	console.log(animal);
+	return animal;
 }
 
 var index = 0;
 
 setInterval(() => {
-        func();
-		
+        let chosenOne = func();
+    	console.log(chosenOne);
+
 		index = index + 1;
 		if (index == 5){
 			process.exit(0);
