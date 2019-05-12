e4231f8c90bfa760588c98e0e1d986b6cea8d4d8
BENROOKOUT\Ben <ben@rookout.com>
2019-05-12 17:16:36 +0300

    test

--- examples/example_app.js
+++ examples/example_app.js
@@ -3,7 +3,7 @@ console.log(new Date(Date.now()).toLocaleString() +  " HelloWorld");
 const animals = ["Rook",
     "Big Rook",
     "Small Rook"];
-	
+
 function func() {
 	let animal = animals[Math.floor(Math.random()*animals.length)];
 	console.log(animal);
@@ -13,9 +13,6 @@ var index = 0;
 
 setInterval(() => {
         func();
-		
+
 		index = index + 1;
-		if (index == 5){
-			process.exit(0);
-		}
     }, 1500);
